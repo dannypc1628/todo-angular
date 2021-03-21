87140c07f1bff3b68916f8653fbe68be4ab2290c
dannyLiu <h152074716@gmail.com>
2021-03-21 14:22:47 +0800

    待辦事項 create與迴圈顯示事項

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './interface/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-angular';
+
+  todos: Todo[] = [];
+  todoInput = '';
+  private;
+
+  ngOnInit(): void {}
+
+  create() {
+    const newTodo: Todo = {
+      title: this.todoInput,
+      id: Date.now(),
+      order: this.todos.length + 1,
+      tomatoCount: 0,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+    this.todoInput="";
+  }
+
+  private editTodo() {}
 }
